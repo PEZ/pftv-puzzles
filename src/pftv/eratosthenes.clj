dcecd7a81ccc0cc9c28683129d56e9979ce88467
Peter Strömberg <pez@pezius.com>
2019-02-19 18:19:10 +0100

    Add some more info from Wikipedia

--- src/pftv/eratosthenes.clj
+++ src/pftv/eratosthenes.clj
@@ -1,6 +1,6 @@
 (ns eratosthenes)
 
-
+; From https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 ; 1. Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
 ; 2. Initially, let p equal 2, the smallest prime number.
 ; 3. Enumerate the multiples of p by counting in increments of p from 2p to n, and mark them in the list (these will be 2p, 3p, 4p, ...; the p itself should not be marked).
@@ -23,5 +23,8 @@
         (recur (conj found-primes (first remaining)) (rest remaining))))))
 
 (comment
+  (sieve 500)
   (not-divisable-by 3 3)
-  (sieve 500))
+  (not-divisable-by 4 3))
+
+; As a refinement, it is sufficient to mark the numbers in step 3 starting from p2, as all the smaller multiples of p will have already been marked at that point. This means that the algorithm is allowed to terminate in step 4 when p2 is greater than n
