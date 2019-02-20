9793aebb4f9a05ba8c2e2eed7ab9ca1925a4bb6e
Peter Strömberg <pez@pezius.com>
2019-02-20 06:56:19 +0100

    Some tracing output to see what's going on

--- src/pftv/eratosthenes.clj
+++ src/pftv/eratosthenes.clj
@@ -3,9 +3,14 @@
 ; From https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 ; 1. Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
 ; 2. Initially, let p equal 2, the smallest prime number.
-; 3. Enumerate the multiples of p by counting in increments of p from 2p to n, and mark them in the list (these will be 2p, 3p, 4p, ...; the p itself should not be marked).
-; 4. Find the first number greater than p in the list that is not marked. If there was no such number, stop. Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
-; 5. When the algorithm terminates, the numbers remaining not marked in the list are all the primes below n.
+; 3. Enumerate the multiples of p by counting in increments of p from 2p to n,
+;    and mark them in the list (these will be 2p, 3p, 4p, ...;
+;    the p itself should not be marked).
+; 4. Find the first number greater than p in the list that is not marked.
+;    If there was no such number, stop. Otherwise, let p now equal this
+;    new number (which is the next prime), and repeat from step 3.
+; 5. When the algorithm terminates, the numbers remaining not marked in the
+;    list are all the primes below n.
 
 (defn not-divisible-by [n d]
   (when-not (integer? (/ n d))
@@ -18,12 +23,13 @@
           remaining (->> candidates
                          (map #(not-divisible-by % highest-found-prime))
                          (remove nil?))]
+      (println candidates)
       (if (= remaining candidates)
         (concat found-primes candidates)
         (recur (conj found-primes (first remaining)) (rest remaining))))))
 
 (comment
-  (sieve 1000))
+  (sieve 70))
 
 ; As a refinement, it is sufficient to mark the numbers in step 3 starting from p2, as all the smaller multiples of p will have already been marked at that point. This means that the algorithm is allowed to terminate in step 4 when p2 is greater than n
 
@@ -32,13 +38,14 @@
     (loop [found-primes [2]
            candidates (range 3 (inc n))]
       (let [highest-found-prime (last found-primes)]
+        (println highest-found-prime found-primes candidates)
         (if (> highest-found-prime sqrt-n)
           (concat found-primes candidates)
           (let [remaining
                 (->> candidates
-                  (map #(not-divisible-by % highest-found-prime))
-                  (remove nil?))]
+                     (map #(not-divisible-by % highest-found-prime))
+                     (remove nil?))]
             (recur (conj found-primes (first remaining)) (rest remaining))))))))
 
 (comment
-  (sieve-refined 1000))
+  (sieve-refined 70))
