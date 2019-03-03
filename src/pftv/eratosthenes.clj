f6de271c690d5caae12bfe191883ef46c251e6b9
Peter Strömberg <pez@pezius.com>
2019-03-03 22:06:32 +0100

    Add palprimes fn

--- src/pftv/eratosthenes.clj
+++ src/pftv/eratosthenes.clj
@@ -62,7 +62,7 @@
 
 (use 'clojure.set)
 
-(defn sieve2 [n]
+(defn sieve2 [^long n]
   (if (< n 2)
     ()
     (let [sqrt-n (Math/sqrt n)]
@@ -72,9 +72,41 @@
           (concat '(2) (sort primes))
           (recur (difference primes (set (range (* p p) n (+ p p)))) (+ p 2)))))))
 
+; def reverse(self, x):
+;     rev = 0
+;     while x > 0:
+;         end = x%10
+;         rev = rev *10 + end
+;         x = x//10
+;     return rev  
+
+(defn mirror [x]
+  (loop [m 0
+         p x]
+        (if-not (> p 0)
+          m
+          (recur (+ (* m 10) (mod p 10)) (int (/ p 10))))))
+
+(defn palprimes [n]
+  (filter #(= % (mirror %)) (sieve n)))
+
+(comment
+  (int (/ 123 10))
+  (mod 123 10)
+  (mirror 12345)
+  (sieve 1000)
+  ((set [1 2 3]) 4)
+  (count (palprimes 1000))
+  (time (str "palprimes: " (count (palprimes 1000000)))))
+
+
 (comment
   (sieve2 30)
-  (time (str "sieve2: " (count (sieve2 1000000)) " primes"))
+
+    
+  (time (str "sieve1: " (count (sieve1 100000)) " primes"))
+  (with-progress-reporting (quick-bench (sieve1 100000) :verbose))
+  (time (str "sieve2:  " (count (sieve2 100000)) " primes"))
   (with-progress-reporting (quick-bench (sieve2 100000) :verbose)))
 
 
@@ -423,5 +455,5 @@
   (run-test primes-to-n)   ; 410   ms
   (run-test sieve1)        ; 125   ms
   (run-test sieve2)        ; 115   ms
-  (run-test sieve-2)       ;  41   ms
-  )
+  (run-test sieve-2))       ;  41   ms
+  
