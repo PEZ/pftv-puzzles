dd04d8792707da6cec888015b7ee3b55945505df
Peter Strömberg <pez@pezius.com>
2019-03-11 23:02:04 +0100

    Add non-multi-arity version of rle

--- src/pftv/run_length.clj
+++ src/pftv/run_length.clj
@@ -1,16 +1,14 @@
 (ns pftv.run-length)
 
-;; https://stackoverflow.com/questions/4830900/how-do-i-find-the-index-of-an-item-in-a-vector
-
-(defn rle
+(defn rle-first-try
   ([c]
    (rle (lazy-seq) c))
   ([e c]
    (if (empty? c)
      e
      (let [p (first (take 1 c))
-           ec (partition-by #(not= p %) c)
-           l (count (first (take 1 ec)))]
+           e|c (partition-by #(not= p %) c)
+           l (count (first (take 1 e|c)))]
        (rle
         (lazy-cat e [[l p]])
         (drop l c))))))
@@ -21,22 +19,39 @@
   ([e c]
    (if (empty? c)
      e
-     (let [p (first (take 1 c))
-           ec (partition-by #(not= p %) c)
-           l (count (first (take 1 ec)))]
+     (let [p (first c)
+           l (or (first (keep-indexed #(when (not= %2 p) %1) c)) (count c))]
        (rle
         (lazy-cat e [[l p]])
         (drop l c))))))
+
+
+(defn rle
+  [c]
+  ((fn encode [e c]
+     (if (empty? c)
+       e
+       (let [p (first c)
+             l (or (first (keep-indexed #(when (not= %2 p) %1) c)) (count c))]
+         (encode
+          (lazy-cat e [[l p]])
+          (drop l c)))))
+   (lazy-seq) c))
+
+(defn rld [e]
+  (mapcat #(repeat (first %) (second %)) e))
 
 
 (comment
+  (count (rle-first-try (range 1000)))
   (= (rle [:a :a :a :b :c :d :d :d :d])
      '([3 :a] [1 :b] [1 :c] [4 :d]))
-  (type (rle [:a :a :a :b :c :d :d :d :d]))
-  (rld '([3 :a] [1 :b] [1 :c] [4 :d]))
+  (rld '([3 :a] [1 :b] [1 :c] [4 :d] [2 "foo"]))
   ;=> (:a :a :a :b :c :d :d :d :d))
 
   (def c [:a :a :a :b :c :d :d :d :d])
+  (drop nil c)
+  (keep-indexed #(when (not= %2 :d) %1) [:d :d :d])
   (drop 4 c)
   (->> c
        (partition-all 2)
