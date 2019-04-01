201f45f8cf6fbc252214354bf639803fcb690503
Peter Strömberg <pez@pezius.com>
2019-04-01 17:27:41 +0200

    Return vector if vector in

--- src/pftv/remove_nth.clj
+++ src/pftv/remove_nth.clj
@@ -2,20 +2,17 @@
 
 (use 'criterium.core)
 
-;; Suspecting Eric is pulling my leg here, but anyway
 (defn remove-at [n c]
-  (concat (take n c) (drop (inc n) c)))
+  ((if (vector? c) (comp vec concat) concat) (take n c) (drop (inc n) c)))
 
 (comment
-  (remove-at 3 [1 2 3 4 5 6])
-  (remove-at 3 '(1 2 3 4 5 6))
-  (with-progress-reporting (quick-bench (remove-at 1000000 (range 1 2000000))))
-  (remove-at 3 [])
-  (remove-at 3 [1])
-  (remove-at 3 [1 2])
-  (remove-at 3 [1 2 3])
-  (remove-at 3 [1 2 3 4])
-  (remove-at 6 [1 2 3 4 5 6])
-  (take 6 (range 10))
-
-  (lazy-cat (seq ["lazy-cat" "is" "my" "favorite" "function"])))
+  (remove-at 3 [1 2 3 4 5 6])  ; => [1 2 3 5 6]
+  (remove-at 3 '(1 2 3 4 5 6)) ; => (1 2 3 5 6)
+  (remove-at 3 (range 1 7))    ; => (1 2 3 5 6)
+  (remove-at 3 [])             ; => []
+  (remove-at 3 [1])            ; => [1]
+  (remove-at 0 [1 2])          ; => [2]
+  (remove-at 1 [1 2])          ; => [1]
+  (remove-at -1 [1 2])         ; => [1 2]
+  (with-progress-reporting (quick-bench (remove-at 10000 (range 1 20000))))
+  (with-progress-reporting (quick-bench (remove-at 1000000 (range 1 2000000)))))
