212e92941cfa7bc7509a3fd3a58a718673b77eae
Peter Strömberg <pez@pezius.com>
2019-04-01 19:02:17 +0200

    Add remove-at version using map-indexed

--- src/pftv/remove_nth.clj
+++ src/pftv/remove_nth.clj
@@ -5,6 +5,10 @@
 (defn remove-at [n c]
   ((if (vector? c) (comp vec concat) concat) (take n c) (drop (inc n) c)))
 
+(defn remove-at [n c]
+  ((if (vector? c) (comp vec (partial remove nil?)) (partial remove nil?))
+   (map-indexed (fn [i item] (when (not= i n) item)) c)))
+
 (comment
   (remove-at 3 [1 2 3 4 5 6])  ; => [1 2 3 5 6]
   (remove-at 3 '(1 2 3 4 5 6)) ; => (1 2 3 5 6)
@@ -15,4 +19,6 @@
   (remove-at 1 [1 2])          ; => [1]
   (remove-at -1 [1 2])         ; => [1 2]
   (with-progress-reporting (quick-bench (remove-at 10000 (range 1 20000))))
-  (with-progress-reporting (quick-bench (remove-at 1000000 (range 1 2000000)))))
+  (with-progress-reporting (quick-bench (count (remove-at 1000000 (range 1 2000000)))))
+  (let [v (vec (range 1 2000000))]
+    (with-progress-reporting (quick-bench (count (remove-at 1000000 v))))))
