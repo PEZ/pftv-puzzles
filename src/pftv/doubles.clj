5d0714c7ec5512770884c7d1f540d1dc1eb05783
Peter Strömberg <pez@pezius.com>
2020-01-06 23:11:41 +0100

    Add more wats to check for dups

--- src/pftv/doubles.clj
+++ src/pftv/doubles.clj
@@ -1,11 +1,27 @@
 (ns pftv.doubles)
 
-(defn duplicate-letter?
+(defn duplicate-letter-1?
   [s]
   (boolean
    (some->> (partition 2 1 s)
             (filter (partial apply =))
             not-empty)))
+
+(defn duplicate-letter-2?
+  [s]
+  (boolean
+   (some->> (partition-by identity s)
+            (group-by count)
+            keys
+            (reduce +)
+            (< 1))))
+
+(defn duplicate-letter?
+  [s]
+  (boolean
+   (->> (map = s (rest s))
+        (drop-while false?)
+        first)))
 
 (comment
   (duplicate-letter? "Hello world")
