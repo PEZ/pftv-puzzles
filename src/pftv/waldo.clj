ab19247d09132c638fc44ad98653bd50186af13c
Peter Strömberg <pez@pezius.com>
2020-01-18 12:30:55 +0100

    Add waldo finder

---
+++ src/pftv/waldo.clj
@@ -0,0 +1,58 @@
+(ns pftv.waldo
+  (:require [clojure.test :refer [is]]))
+
+(defn wheres-waldo
+  "Find the path to waldo"
+  {:test (fn []
+           (is (= [7 1]
+                  (wheres-waldo :W ;; look for :W
+                                [[:A :B :C]
+                                 [:D :E :F]
+                                 [:G :H :I]
+                                 [:J :K :L]
+                                 [:M :N :O]
+                                 [:P :Q :R]
+                                 [:S :T :U]
+                                 [:V :W :X]
+                                 [:Y :and :Z]])))
+           (is (= nil (wheres-waldo :W [[:A :B :C]])))
+           (is (= [1 2]
+                  (wheres-waldo [:i :am :waldo]
+                                [[:a :b "ceee"]
+                                 [[:i :am :not :waldo] [:i :am :spartacus] [:i :am :waldo]]
+                                 ["d" "e" 6]]))))}
+  [waldo grid]
+  (->> (map-indexed (fn [i v] [i (.indexOf v waldo)]) grid)
+       (filter #(not= -1 (second %)))
+       first)
+  )
+
+(defn wheres-waldo-bonus
+  "Find the path to waldo"
+  {:test (fn []
+           (is (= [7 1]
+                  (wheres-waldo-bonus :W ;; look for :W
+                                  [[:A :B :C]
+                                   [:D :E :F]
+                                   [:G :H :I]
+                                   [:J :K :L]
+                                   [:M :N :O]
+                                   [:P :Q :R]
+                                   [:S :T :U]
+                                   [:V [:W] :X]
+                                   [:Y :and :Z]])))
+           (is (= nil (wheres-waldo-bonus :W [[:A :B :C]])))
+           (is (= [1]
+                  (wheres-waldo-bonus :W [:V :W :X])))
+           (is (= [1 2]
+                  (wheres-waldo-bonus [:i :am :waldo]
+                                  [[:a :b "ceee"]
+                                   [[:i :am :not :waldo] [:i :am :spartacus] [:hello [:i :am :waldo] [:who :are :you?]]]
+                                   ["d" "e" 6]]))))}
+  ([waldo vektor]
+   (wheres-waldo-bonus waldo vektor []))
+  ([waldo vektor path]
+   (let [i (.indexOf vektor waldo)]
+     (if (> i -1)
+       (conj path i)
+       "foo"))))
