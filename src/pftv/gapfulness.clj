5eeb36dcc0f31dd4be926d13c71a1cb93aadbb90
Peter Strömberg <pez@pezius.com>
2020-01-22 13:56:03 +0100

    Add gapful

---
+++ src/pftv/gapfulness.clj
@@ -0,0 +1,19 @@
+(ns pftv.gapfulness)
+
+(defn gapful [num]
+  (loop [candidates [num num]]
+    (let [gapful? (fn [n]
+                    (when (and n (> n 99))
+                      (let [d (as-> n v
+                                (str v)
+                                (str (first v) (last v))
+                                (read-string v))]
+                        (= (mod n d) 0))))
+          recruit (first (filter gapful? candidates))]
+      (if recruit
+        recruit
+        (recur [(dec (first candidates)) (inc (last candidates))])))))
+
+
+(comment
+  (gapful 113))
