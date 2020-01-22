6eaedf159e435c91926ec444c2c793ba2e2e49be
Peter Strömberg <pez@pezius.com>
2020-01-22 15:44:23 +0100

    Short curcuit < 100

--- src/pftv/gapfulness.clj
+++ src/pftv/gapfulness.clj
@@ -1,19 +1,21 @@
 (ns pftv.gapfulness)
 
 (defn gapful [num]
-  (loop [candidates [num num]]
-    (let [gapful? (fn [n]
-                    (when (and n (> n 99))
-                      (let [d (as-> n v
-                                (str v)
-                                (str (first v) (last v))
-                                (read-string v))]
-                        (= (mod n d) 0))))
-          recruit (first (filter gapful? candidates))]
-      (if recruit
-        recruit
-        (recur [(dec (first candidates)) (inc (last candidates))])))))
+  (if (< num 100)
+    100
+    (loop [candidates [num num]]
+      (let [gapful? (fn [n]
+                      (when (and n (> n 99))
+                        (let [d (as-> n v
+                                  (str v)
+                                  (str (first v) (last v))
+                                  (read-string v))]
+                          (= (mod n d) 0))))
+            recruit (first (filter gapful? candidates))]
+        (if recruit
+          recruit
+          (recur [(dec (first candidates)) (inc (last candidates))]))))))
 
 
 (comment
-  (gapful 113))
+  (gapful -57657104))
